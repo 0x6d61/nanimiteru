08c62774bc5b65b80fab82aaf2e5b2f60b54a089
0x6d61 <0x6d61@gmail.com>
2019-12-04 17:58:24 +0900

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,50 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>今期は何見てる?</h1>
+    {{ content }}
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+//import {fetchOgpImage} from "./store"
+
+const data = new Date();
+const year = data.getFullYear();
+const month = data.getMonth();
+//fetchOgpImage('http://0x6d61.github.io')
+const getSeason = month => {
+  if (1 <= month && month <= 3) {
+    return 1;
+  } else if (4 <= month && month <= 6) {
+    return 2;
+  } else if (7 <= month && month <= 9) {
+    return 3;
+  } else {
+    return 4;
+  }
+};
 
 export default {
   name: "app",
-  components: {
-    HelloWorld
+  data() {
+    return {
+      year: year,
+      season: getSeason(month)
+    };
+  },
+  computed: {
+    content() {
+      this.fetchAnimeList();
+      return this.$store.state.animeList;
+    }
+  },
+  methods: {
+    fetchAnimeList() {
+      this.$store.dispatch("fetchAnimeList", {
+        year: this.year,
+        season: this.season
+      });
+    }
   }
 };
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,53 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import axios from "axios";
+import cheerio from "cheerio";
+
+export const fetchOgpIameg = async url => {
+  const html = await axios.get(url);
+  const $ = cheerio.load(html.data);
+  $("head meta").each((i, el) => {
+    const property = $(el).attr("property");
+    if (property === "og:image") {
+      return $(el).attr("content");
+    }
+  });
+};
+fetchOgpIameg("http://www.nhk.or.jp/anime/radiant/");
+const fetchAnimeList = async (year, season) => {
+  const atnimeTitle = await axios.get(
+    `http://api.moemoe.tokyo/anime/v1/master/${year}/${season}`
+  );
+
+  return atnimeTitle.data.map(async i => {
+    return {
+      title: i.title,
+      public_url: i.public_url,
+      image_url: await fetchOgpIameg(i.public_url)
+    };
+  });
+};
+
+const animeList = {
+  state: {
+    animeList: {}
+  },
+  mutations: {
+    getAnimeList(state, data) {
+      state.animeList = data;
+    }
+  },
+  actions: {
+    async fetchAnimeList({ commit }, data) {
+      const animeList = await fetchAnimeList(data.year, data.season);
+      commit("getAnimeList", animeList);
+    }
+  }
+};
 
 Vue.use(Vuex);
-
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+  modules: {
+    animeList
+  }
 });
